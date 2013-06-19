8381b7e41a21aab6b63446f8ce9297a963447fb5
blibotics <blibotics@gmail.com>
2013-06-18 21:53:49 -0400

    Updated js file to include game logic

--- js/pigLatin.js
+++ js/pigLatin.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
-
+	var submitBtn = $("#submit");
+	submitBtn.click(function(){
+		var inputValue = $("#word").val();
+		var inputArray = inputValue.split("");
+		if (inputArray[0].match(/[aeiouAEIOU]/)) {
+			$("#return_value").text(inputValue);
+		}
+		else {
+			var letter = inputArray[0];
+			inputArray.push(letter);
+			inputArray.shift();
+			var newWord = inputArray.join("");
+			var finalWord = newWord+"ay";
+			$("#return_value").text(finalWord);
+		}
+	});
 });
