67931ae3d762245b6d53e0a976d24fc800fa081c
blibotics <blibotics@gmail.com>
2013-06-19 18:28:47 -0400

    Added on Enter Key functionality and refactored code

--- js/pigLatin.js
+++ js/pigLatin.js
@@ -1,6 +1,19 @@
 $(document).ready(function(){
+
+//on button click
 	var submitBtn = $("#submit");
-	submitBtn.click(function(){
+	submitBtn.click(piglatin);
+
+//on hitting enter key
+	$("#word").keyup(function(event){
+		var code = (event.keyCode ? event.keyCode : event.which);
+			if (code == 13) {
+				piglatin();
+		}
+	});
+
+// created function for events to call
+	function piglatin(){
 		var inputValue = $("#word").val();
 		var inputArray = inputValue.split("");
 		if (inputArray[0].match(/[aeiouAEIOU]/)) {
@@ -14,5 +27,7 @@ $(document).ready(function(){
 			var finalWord = newWord+"ay";
 			$("#return_value").text('"' + finalWord +'"');
 		}
-	});
+	}
 });
+
+
